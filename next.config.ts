71101bbe318faa047e692c0c6a05f2193e788e8e
dajeong <uwhami@gmail.com>
2025-05-22 01:02:22 +0900

    Deploy static site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+    output: "export",
+    basePath: isProd ? "/uwhami.github.io" : "",
+    assetPrefix: isProd ? "/uwhami.github.io/" : "",
 };
 
 export default nextConfig;
