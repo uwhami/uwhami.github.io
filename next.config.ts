4d0c87c9168170422719d049ee4d70b9c5b6bc92
djkim <djkim12@douzone.com>
2025-05-22 09:57:37 +0900

    Fix Settings

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
     output: "export",
-    basePath: isProd ? "/uwhami.github.io" : "",
-    assetPrefix: isProd ? "/uwhami.github.io/" : "",
+    images: { // next/image 사용 시 추가
+        unoptimized: true,
+    },
 };
-
 export default nextConfig;
